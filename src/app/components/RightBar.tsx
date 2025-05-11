e44cc70cec0c69937a03f8902cb1163c8cba5b9c
Reydwen <raydantagub232@gmail.com>
2025-05-11 23:50:01 +0800

    Initial commit

--- src/app/components/RightBar.tsx
+++ src/app/components/RightBar.tsx
@@ -1,14 +1,7 @@
 import Link from "next/link";
-import PopularTags from "./PopularTags";
-import Recommendations from "./Recommendations";
-import Search from "./Search";
 
 const RightBar = () => {
   return (
-    <div className="pt-4 flex flex-col gap-4 sticky top-0 h-max">
-      <Search />
-      <PopularTags />
-      <Recommendations />
       <div className="text-textGray text-sm flex gap-x-4 flex-wrap">
         <Link href="/">Terms of Service</Link>
         <Link href="/">Privacy Policy</Link>
@@ -17,7 +10,7 @@ const RightBar = () => {
         <Link href="/">Ads Info</Link>
         <span>© 2025 L Corp.</span>
       </div>
-    </div>
+
   );
 };
 
