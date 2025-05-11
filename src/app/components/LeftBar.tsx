873b001a4bc2cc638921594bb8ae7302754ebdb9
Reydwen <raydantagub232@gmail.com>
2025-05-11 23:30:22 +0800

    Initial commit

---
+++ src/app/components/LeftBar.tsx
@@ -0,0 +1,56 @@
+import Image from "next/image";
+ import Link from "next/link";
+  const menuList = [
+    { id: 1, name: "Homepage", link: "/", icon: "home.svg", },
+
+    { id: 2, name: "Explore", link: "/", icon: "explore.svg", },
+
+    { id: 3, name: "Notification", link: "/", icon: "notification.svg", },
+
+    { id: 4, name: "Messages", link: "/", icon: "message.svg", },
+
+    { id: 5, name: "Bookmarks", link: "/", icon: "bookmark.svg", },
+
+    { id: 6, name: "Jobs", link: "/", icon: "job.svg", },
+
+    { id: 7, name: "Community", link: "/", icon: "community.svg", },
+
+    { id: 9, name: "Profile", link: "/", icon: "profile.svg", },
+
+    { id: 10, name: "More", link: "/", icon: "more.svg", }, ];
+
+    const Leftbar = () => {
+        return (
+             <div className="h-screen sticky top-0 flex flex-col justify-between pt-2 pb-8">
+            <div className="flex flex-col gap-4 text-lg items-center xxl:items-start">
+                <Link href="/" className="p-2 rounded-full hover:bg-purple-400"> 
+                <Image src="icons/logo.svg" alt="logo" width={24} height={24} /> 
+                </Link> 
+                <div className="flex flex-col"> {menuList.map((item) => ( 
+                    <Link href={item.link} className="p-2 rounded-full hover:bg-purple-400 flex items-center gap-4" key={item.id} >
+                         <Image src={`icons/${item.icon}`} alt={item.name} width={24} height={24} /> 
+                    <span className="hidden xxl:inline group-hover:text-black hover:text-bold">{item.name}</span> 
+                    </Link> ))} 
+                    </div> 
+                    <Link href="/" className=" bg-purple-600 text-white rounded-full w-12 h-12 flex items-center justify-center xxl:hidden"> 
+                    <Image src="icons/post.svg" alt="new post" width={24} height={24} /> 
+                    </Link> 
+                    <Link href="/" className="hidden xxl:block bg-purple-600 text-black hover:text-white rounded-full font-bold py-2 px-20"> Post 
+                    </Link> 
+                    </div> 
+                    <div className="flex items-center justify-between"> 
+                        <div className="flex items-center gap-2"> 
+                            <div className="w-10 h-10 relative rounded-full overflow-hidden"> 
+                                <Image src="/general/images (11).jpeg" alt="John Snow" layout="fill" objectFit="cover" /> </div> 
+                                <div className="hidden xxl:flex flex-col"> 
+                                    <span className="font-bold">User</span> 
+                                    <span className="text-sm text-textGray">raydantagub@gmail.com</span> 
+                                    </div> 
+                                    </div> 
+                                    <div className="hidden xxl:block cursor-pointer font-bold">...</div> 
+                                    </div> 
+                                    </div>
+                                     
+                                    );
+
+                                    }; export default Leftbar;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
+"use client";
+import Image from 'next/image';
+import { IKImage } from "imagekitio-next";
+
+const urlEndpoint = process.env.NEXT_PUBLIC_URL_ENDPOINT;
+
 const Homepage = () => {
   return (
-    <div className=''>Homepage</div>
-  )
+    <div className="homepage-feed">
+      <div className="post">
+        <Image src="/general/post.jpeg" alt="Post image" width={600} height={200} />
+        <p className="post-description">This is a sample post description.</p>
+      </div>
+    </div>
+  );
 }
 
-export default Homepage
+export default Homepage;
